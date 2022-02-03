72492a74bbfb847c889aa52d0bc5a5e4cc134d7d
Nazmul Islam Ananto <nazmulislamananto@gmail.com>
2022-02-03 13:53:15 +0600

    tried some queries and learned how to handle body parameters

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const res = require("express/lib/response");
 const router = require('express-promise-router')();
 
+const app = express();
+app.use(express.json());
+
 const queries = require('./queries.js');
 
 /// express ===================================
+
 router.get("/all", async function(req, res){
     return res.status(200).json(await queries.getPeople());
 });
@@ -14,11 +18,32 @@ router.get("/:id", async function(req, res){
     return res.status(200).json(await queries.getPerson(personId));
 });
 
-const app = express();
+
+
+
+router.post("/registration/:page", async function(req, res){
+    const a = req.body.a;
+    const b = req.body.b;
+    // console.log(a, b);
+    console.log(req.body);
+    console.log(req.params.page);
+    return res.status(200).json({
+        success: 'true',
+        a : a,
+        b : b
+    });
+});
+
+
+
+
+
+//// server ====================================
+
 app.use(router);
 
-app.listen(3000, function(){
-    console.log("server started at port 3000");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, function(){
+    console.log(`server started at port ${PORT}`);
 })
-
-
