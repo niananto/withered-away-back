d8803be520e206ed02160c43ba46e2c757b59b0a
Nazmul Islam Ananto <nazmulislamananto@gmail.com>
2022-02-04 13:27:56 +0600

    added new queries and apis for registration form

--- index.js
+++ index.js
@@ -1,42 +1,62 @@
 const express = require("express");
 const res = require("express/lib/response");
-const router = require('express-promise-router')();
+const router = require("express-promise-router")();
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
-const queries = require('./queries.js');
+const queries = require("./queries.js");
 
 /// express ===================================
 
-router.get("/all", async function(req, res){
-    return res.status(200).json(await queries.getPeople());
-});
-
-router.get("/:id", async function(req, res){
+router.get("/:id", async function (req, res) {
     const personId = req.params.id;
     return res.status(200).json(await queries.getPerson(personId));
 });
 
-
-
-
-router.post("/registration/:page", async function(req, res){
-    const a = req.body.a;
-    const b = req.body.b;
-    // console.log(a, b);
-    console.log(req.body);
-    console.log(req.params.page);
-    return res.status(200).json({
-        success: 'true',
-        a : a,
-        b : b
-    });
+router.get("/all/:tableName", async function (req, res) {
+    const tableName = req.params.tableName;
+    console.log(req.params);
+    return res.status(200).json(await queries.getAll(tableName));
 });
 
+let registration = {};
+router.post("/registration/:page", async function (req, res) {
+    if (req.params.page == 1) {
+        registration.name = req.body.firstName + " " + req.body.lastName; // any string
+        registration.sex = req.body.sex; // male, female
+        registration.dateOfBirth = req.body.dateOfBirth; // DD-MON-YYYY
+    } else if (req.params.page == 2) {
+        registration.contactName = req.body.contactName;
+        registration.contactPhoneNo = req.body.contactPhoneNo;
+        registration.contactRelationship = req.body.contactRelationship;
+        registration.contactAddress = req.body.contactAddress;
+    } else if (req.params.page == 3) {
+        registration.diseases = req.body.diseases; // list of diseases
+        registration.medicines = req.body.medicines; // list of medicines
+        registration.height = req.body.height;
+        registration.weight = req.body.weight;
+        registration.bloodGroup = req.body.bloodGroup;
+        registration.vaccines = req.body.vaccines; // list of vaccines
+        registration.dissabilities = req.body.dissabilities; // list
+        registration.allergies = req.body.allergies;
+        registration.healthCondition = req.body.healthCondition;
+    } else if (req.params.page == 4) {
+        registration.songs = req.body.songs; // list
+        registration.movies = req.body.movies; // list
+        registration.games = req.body.games; // list
+    } else if (req.params.page == 5) {
+        registration.bankAccountNo = req.body.bankAccountNo;
+        registration.balance = req.body.balance;
+        registration.membershipId = req.body.membershipId;
+    }
 
-
-
+    return res.status(200).json({
+        success: "true",
+        registration,
+    });
+});
 
 //// server ====================================
 
@@ -44,6 +64,6 @@ app.use(router);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log(`server started at port ${PORT}`);
-})
+});
--- queries.js
+++ queries.js
@@ -3,46 +3,42 @@ oracledb.outFormat = oracledb.OBJECT;
 
 var connection = undefined;
 
-async function db_query(query, params){
-    if(connection === undefined){
+async function db_query(query, params) {
+    if (connection === undefined) {
         connection = await oracledb.getConnection({
-            // user: 'c##starlord',
-            // password: 'gamora',
-            // connectString: 'localhost/orcl'
-            user: 'TFUSER22',
-            password: '0919db77',
-            connectString: '103.94.135.201:1521/orclcdb.localdomain'
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            connectString: process.env.DB_CONNECTIONSTRING,
         });
         console.log("database connected successfully");
     }
 
-    try{
+    try {
         let result = await connection.execute(query, params);
         return {
             success: true,
             data: result.rows,
-        } 
-    }catch(e){
+        };
+    } catch (e) {
         return {
-            success : false,
-        }
+            success: false,
+        };
     }
 }
 
-async function getPerson(id){
+async function getPerson(id) {
     const q = `SELECT * FROM PEOPLE WHERE ID=:1`;
     const params = [id];
     const result = await db_query(q, params);
     return result;
 }
 
-async function getPeople(){
-    const q = `SELECT * FROM PEOPLE`;
+async function getAll(tableName) {
+    const q = `SELECT * FROM ${tableName}`;
     const params = [];
     const result = await db_query(q, params);
     return result;
 }
 
-
 exports.getPerson = getPerson;
-exports.getPeople = getPeople;
+exports.getAll = getAll;
